aaf403f2825f3aa9fba210b1252a4f1dfcbd3af5
Sem987 <94830216+Sem987@users.noreply.github.com>
2025-08-19 15:58:56 +0200

    About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import {ScrollTrigger, SplitText} from "gsap/all";
 import gsap from "gsap";
-import {Navbar} from "./components/navbar.jsx";
+import {Navbar} from "./components/Navbar.jsx";
 import {Hero} from "./components/Hero.jsx";
-import {Cocktails} from "./components/cocktails.jsx";
+import {Cocktails} from "./components/Cocktails.jsx";
+import {About} from "./components/About.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,6 +14,7 @@ export const App = () => {
             <Navbar />
             <Hero />
             <Cocktails />
+            <About />
         </main>
     )
 }
